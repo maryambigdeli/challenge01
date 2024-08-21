dc30c431799c8ca8fa1769ff00d0f0c358ad4a2f
maryam bigdeli <maryambigdely66@gmail.com>
2024-08-21 19:22:14 +0330

    my change

--- challenge01/Program.cs
+++ challenge01/Program.cs
@@ -10,6 +10,11 @@ namespace challenge01
     {
         static void Main(string[] args)
         {
+            
+            for (int i=0; i<10; i++ )
+            {
+                Console.WriteLine("challenge01");
+            }
         }
     }
 }
